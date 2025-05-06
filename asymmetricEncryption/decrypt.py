1e61ad1756e4e67b6978705e7c29ea9eeff4d76f
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2025-05-06 14:17:40 +1000

    Update decrypt.py

--- asymmetricEncryption/decrypt.py
+++ asymmetricEncryption/decrypt.py
@@ -1,14 +1,19 @@
 from ecies import decrypt
 import binascii
+import os, sys, inspect #For dynamic filepaths
+
+#Find the execution path and join it with the direct reference
+def execution_path(filename):
+  return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)	
 
 def decryptMessage():
   # Read in and un-hex our private key
-  with open('textfiles/secret.txt') as f:
+  with open(execution_path('textfiles/secret.txt')) as f:
     for line in f:
       sk_bytes = binascii.unhexlify(''.join(line.split()))
 
   # Read in and un-hex our message
-  with open('textfiles/encrypted.txt') as f:
+  with open(execution_path('textfiles/encrypted.txt')) as f:
     for line in f:
       data = binascii.unhexlify(''.join(line.split()))
 
