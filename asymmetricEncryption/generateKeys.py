9f64444ad6c08a4e6dc9f6281f0f7f6aed403a73
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2025-05-06 14:18:35 +1000

    Update generateKeys.py

--- asymmetricEncryption/generateKeys.py
+++ asymmetricEncryption/generateKeys.py
@@ -1,5 +1,9 @@
 from ecies.utils import generate_key
 import binascii
+import os, sys, inspect #For dynamic filepaths
+
+def execution_path(filename):
+  return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)
 
 def makeKeys():
   # Create the key pair, store it into variables
@@ -8,9 +12,9 @@ def makeKeys():
   pk_bytes = eth_k.public_key.format(True)  # hex string
 
   # Encode the byte to a hex value and write out to different files
-  with open('textfiles/secret.txt', 'wb') as f:
+  with open(execution_path('textfiles/secret.txt'), 'wb') as f:
     f.write(binascii.hexlify(sk_bytes))
-  with open('textfiles/public.txt', 'wb') as f:
+  with open(execution_path('textfiles/public.txt'), 'wb') as f:
     f.write(binascii.hexlify(pk_bytes))
 
   print("generated keys!")
