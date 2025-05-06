8042e28f07438a8ab66a9cb6f666664cdea243e9
Micah Chubb <89553508+MicahChubb@users.noreply.github.com>
2025-05-06 14:16:49 +1000

    Update encrypt.py

--- asymmetricEncryption/encrypt.py
+++ asymmetricEncryption/encrypt.py
@@ -1,15 +1,20 @@
 from ecies import encrypt
 import binascii
+import os, sys, inspect #For dynamic filepaths
 
+#Find the execution path and join it with the direct reference
+def execution_path(filename):
+  return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), filename)
+    
 def encryptMessage(data):
     # Read in from our text file, convert from hex to a byte
-    with open('textfiles/public.txt') as f:
+    with open(execution_path('textfiles/public.txt')) as f:
         for line in f:
             pk_hex = binascii.unhexlify(''.join(line.split()))
 
     # Encrypt the test string and then write out to a file as hex
     encryptedMessage = encrypt(pk_hex, data.encode("utf8"))
-    with open('textfiles/encrypted.txt', 'wb') as f:
+    with open(execution_path('textfiles/encrypted.txt'), 'wb') as f:
         f.write(binascii.hexlify(encryptedMessage))
     
     print("encrypted message!")
